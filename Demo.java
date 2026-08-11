d2b3ceb691d00e1a8c7f02e21fb68bfc17440a6f
adilrazaa <adilraza22262@gmail.com>
2026-08-11 22:29:14 +0530

    Rotation count of Rotated sorted array

---
+++ Demo.java
@@ -0,0 +1,21 @@
+// Rotation count of Rotated sorted array
+public class Demo {
+    public static void main(String[] args){
+        int[] arr={3,4,5,6,1,2};
+        System.out.println(count(arr));
+    }
+    static int count(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(arr[mid]>arr[end]){
+                start=mid+1;
+            }
+            else{
+                end=mid;
+            }
+        }
+        return start;
+    }
+}
